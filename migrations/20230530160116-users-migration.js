4e6eb8c0cad00ba9ba3b3d2533ebb76a65df68bc
Rizki Firmansyah <rizkifirmansyah2642@gmail.com>
2023-05-30 23:32:33 +0700

    set up migration

---
+++ migrations/20230530160116-users-migration.js
@@ -0,0 +1,49 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.createTable('users',{
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      fullName: Sequelize.STRING,
+      email : Sequelize.STRING,
+      password : Sequelize.STRING,
+      role: {
+        type: Sequelize.INTEGER,
+        references: {
+          models: 'roles',
+          key: 'id',
+        },
+      height: Sequelize.INTEGER,
+      weight: Sequelize.INTEGER,
+      calories: Sequelize.INTEGER,
+      photoProfile: Sequelize.STRING,
+      phoneNumber: Sequelize.STRING,
+      created_at : Sequelize.DATE,
+      updated_at : Sequelize.DATE
+      }
+    });
+
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.dropTable('users');
+
+  }
+};
---
+++ migrations/20230530160158-bookmarks-migration.js
@@ -0,0 +1,44 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.createTable('bookmarks', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      id_recipe: {
+        type: Sequelize.INTEGER,
+        reference: {
+          model: 'recipes',
+          key: 'id',
+        }
+      },
+      id_user: {
+        type: Sequelize.INTEGER,
+        reference: {
+          model: 'users',
+          key: 'id',
+        }
+      }
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.dropTable('bookmarks');
+  }
+};
